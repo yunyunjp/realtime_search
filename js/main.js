f2398303bbc93cf36f666b63ffd55a356569dc2c
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-17 11:43:22 +0900

    APIからデータ取得

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        items: null,
+        keyword: '',
+        message: ''
     },
     watch: {
 
     },
     created: function() {
-
+      this.keyword = 'JavaScript'
+      this.getAnswer()
     },
     methods: {
-        
+        getAnswer: function() {
+          if(this.keyword === '') {
+              this.items = null
+              return
+          }
+
+          this.message = 'Loading...'
+          var vm = this
+          var params = { page: 1, per_page: 20, query: this.keyword }
+          axios.get('https://qitta.com/api/v2/items', { params })
+          .then(function(response){
+              console.log(response)
+          })
+          .catch(function(error) {
+              vm.message = 'Error!' + error
+          })
+          .finally(function() {
+              vm.message = ''
+          })
+        }
     }
 })
